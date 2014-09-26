04697b943c8cd50c8ad3068f0dd5d411ceda333a
golovin <golovin@voyager>
2014-09-26 22:54:55 +0400

    -

--- MarketBusinessLogic/src/main/java/market/bl/dao/XmlMarketDao.java
+++ MarketBusinessLogic/src/main/java/market/bl/dao/XmlMarketDao.java
@@ -19,20 +19,19 @@ import javax.xml.namespace.QName;
 import javax.xml.transform.stream.StreamSource;
 
 public class XmlMarketDao implements IMarketDao {
-
+    
     private String db_filename = DEFAULT_DB_FILENAME;
-
+    
     public static final String DEFAULT_DB_FILENAME = "build/resource/db_junit.xml";
-
+    
     public XmlMarketDao() {
 	init();
     }
-
+    
     public XmlMarketDao(String fileName) {
 	db_filename = fileName;
 	init();
     }
-
     private void init() {
 	try {
 	    if (!new File("build/resource").exists()) {
@@ -50,7 +49,7 @@ public class XmlMarketDao implements IMarketDao {
     public void saveOrUpdate(Object object) {
 	try {
 	    RootNode rootNode = getFromFile();
-
+	    
 	    String fieldName = object.getClass().getSimpleName();
 	    List fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
 	    if (fieldList == null) {
@@ -61,14 +60,15 @@ public class XmlMarketDao implements IMarketDao {
 	    Iterator<?> iter = fieldList.iterator();
 	    while (iter.hasNext()) {
 		Object fieldObject = iter.next();
-		if (object.getClass().getDeclaredField("id").getInt(object) == fieldObject.getClass().getDeclaredField("id").getInt(fieldObject)) {
+		if (object.getClass().getDeclaredField("id").getInt(object) 
+			== fieldObject.getClass().getDeclaredField("id").getInt(fieldObject)) {
 		    iter.remove();
 		    break;
 		}
 	    }
-	    // TODO: ADD
+	    //TODO: ADD
 	    fieldList.add(object);
-
+	    
 	    saveToFile(rootNode);
 	    System.out.println("Object saved: " + object);
 	} catch (JAXBException e) {
@@ -106,31 +106,31 @@ public class XmlMarketDao implements IMarketDao {
 	}
 
     }
-
+    
     @Override
     public <T> List<T> getAll(Class<T> objectClass) {
-	try {
-	    List<T> retVal = new ArrayList<T>();
-	    RootNode rootNode = getFromFile();
-	    String fieldName = objectClass.getSimpleName();
-	    List fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
-	    if (fieldList == null) {
-		rootNode.getClass().getDeclaredField(fieldName).set(rootNode, new ArrayList());
-		fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
-	    }
+    	try {
+    		List<T> retVal = new ArrayList<T>();
+    	    RootNode rootNode = getFromFile();
+    	    String fieldName = objectClass.getSimpleName();
+    	    List fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
+    	    if (fieldList == null) {
+    		rootNode.getClass().getDeclaredField(fieldName).set(rootNode, new ArrayList());
+    		fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
+    	    }
 
-	    if (fieldList == null) {
-		return null;
-	    }
-	    for (Object fieldObject : fieldList) {
-		retVal.add((T) fieldObject);
-	    }
-	    return retVal;
-	} catch (JAXBException e) {
-	    throw new RuntimeException(e);
-	} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-	    throw new RuntimeException(e);
-	}
+    	    if (fieldList == null) {
+    		return null;
+    	    }
+    	    for (Object fieldObject : fieldList) {
+    		    retVal.add((T) fieldObject);
+    	    }
+    	    return retVal;
+    	} catch (JAXBException e) {
+    	    throw new RuntimeException(e);
+    	} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+    	    throw new RuntimeException(e);
+    	}
     }
 
     @Override
@@ -141,7 +141,8 @@ public class XmlMarketDao implements IMarketDao {
 	    Unmarshaller unmarshaller = jc.createUnmarshaller();
 	    JAXBElement<RootNode> je1 = unmarshaller.unmarshal(xml, RootNode.class);
 	    RootNode rootNode = je1.getValue();
-
+	    
+	    
 	    String fieldName = objectClass.getSimpleName();
 	    List fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
 	    if (fieldList == null) {
@@ -168,13 +169,14 @@ public class XmlMarketDao implements IMarketDao {
 	    throw new RuntimeException(e);
 	}
     }
-
+    
+    
     private void saveToFile(RootNode rootNode) {
 	try {
 	    JAXBContext jc = JAXBContext.newInstance(RootNode.class);
 	    JAXBElement<?> je2 = new JAXBElement(new QName(RootNode.class.getSimpleName()), RootNode.class, rootNode);
 	    Marshaller marshaller = jc.createMarshaller();
-	    // marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+//	    marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
 	    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 	    OutputStream out = new FileOutputStream(new File(db_filename), false);
 	    marshaller.marshal(je2, out);
@@ -184,14 +186,14 @@ public class XmlMarketDao implements IMarketDao {
 	    throw new RuntimeException(e);
 	}
     }
-
+    
     private RootNode getFromFile() throws JAXBException {
-	JAXBContext jc = JAXBContext.newInstance(RootNode.class);
-	StreamSource xml = new StreamSource(new File(db_filename));
-	Unmarshaller unmarshaller = jc.createUnmarshaller();
-	JAXBElement<RootNode> je1 = unmarshaller.unmarshal(xml, RootNode.class);
-	RootNode rootNode = je1.getValue();
-	return rootNode;
+	    JAXBContext jc = JAXBContext.newInstance(RootNode.class);
+	    StreamSource xml = new StreamSource(new File(db_filename));
+	    Unmarshaller unmarshaller = jc.createUnmarshaller();
+	    JAXBElement<RootNode> je1 = unmarshaller.unmarshal(xml, RootNode.class);
+	    RootNode rootNode = je1.getValue();
+	    return rootNode;
     }
 
 }
