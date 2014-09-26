52e95d50d7d1d033e386865aadf07d939f23d61c
golovin <golovin@voyager>
2014-09-26 22:40:47 +0400

    -

--- MarketBusinessLogic/src/main/java/market/bl/dao/XmlMarketDao.java
+++ MarketBusinessLogic/src/main/java/market/bl/dao/XmlMarketDao.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
@@ -19,24 +18,26 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
 import javax.xml.transform.stream.StreamSource;
 
-import market.model.order.Product;
-
 public class XmlMarketDao implements IMarketDao {
-    
+
     private String db_filename = DEFAULT_DB_FILENAME;
-    
+
     public static final String DEFAULT_DB_FILENAME = "build/resource/db_junit.xml";
-    
+
     public XmlMarketDao() {
 	init();
     }
-    
+
     public XmlMarketDao(String fileName) {
 	db_filename = fileName;
 	init();
     }
+
     private void init() {
 	try {
+	    if (!new File("build/resource").exists()) {
+		new File("build/resource").mkdir();
+	    }
 	    FileWriter writer = new FileWriter(new File(db_filename));
 	    writer.flush();
 	    saveToFile(new RootNode());
@@ -49,7 +50,7 @@ public class XmlMarketDao implements IMarketDao {
     public void saveOrUpdate(Object object) {
 	try {
 	    RootNode rootNode = getFromFile();
-	    
+
 	    String fieldName = object.getClass().getSimpleName();
 	    List fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
 	    if (fieldList == null) {
@@ -60,15 +61,14 @@ public class XmlMarketDao implements IMarketDao {
 	    Iterator<?> iter = fieldList.iterator();
 	    while (iter.hasNext()) {
 		Object fieldObject = iter.next();
-		if (object.getClass().getDeclaredField("id").getInt(object) 
-			== fieldObject.getClass().getDeclaredField("id").getInt(fieldObject)) {
+		if (object.getClass().getDeclaredField("id").getInt(object) == fieldObject.getClass().getDeclaredField("id").getInt(fieldObject)) {
 		    iter.remove();
 		    break;
 		}
 	    }
-	    //TODO: ADD
+	    // TODO: ADD
 	    fieldList.add(object);
-	    
+
 	    saveToFile(rootNode);
 	    System.out.println("Object saved: " + object);
 	} catch (JAXBException e) {
@@ -106,31 +106,31 @@ public class XmlMarketDao implements IMarketDao {
 	}
 
     }
-    
+
     @Override
     public <T> List<T> getAll(Class<T> objectClass) {
-    	try {
-    		List<T> retVal = new ArrayList<T>();
-    	    RootNode rootNode = getFromFile();
-    	    String fieldName = objectClass.getSimpleName();
-    	    List fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
-    	    if (fieldList == null) {
-    		rootNode.getClass().getDeclaredField(fieldName).set(rootNode, new ArrayList());
-    		fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
-    	    }
+	try {
+	    List<T> retVal = new ArrayList<T>();
+	    RootNode rootNode = getFromFile();
+	    String fieldName = objectClass.getSimpleName();
+	    List fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
+	    if (fieldList == null) {
+		rootNode.getClass().getDeclaredField(fieldName).set(rootNode, new ArrayList());
+		fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
+	    }
 
-    	    if (fieldList == null) {
-    		return null;
-    	    }
-    	    for (Object fieldObject : fieldList) {
-    		    retVal.add((T) fieldObject);
-    	    }
-    	    return retVal;
-    	} catch (JAXBException e) {
-    	    throw new RuntimeException(e);
-    	} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-    	    throw new RuntimeException(e);
-    	}
+	    if (fieldList == null) {
+		return null;
+	    }
+	    for (Object fieldObject : fieldList) {
+		retVal.add((T) fieldObject);
+	    }
+	    return retVal;
+	} catch (JAXBException e) {
+	    throw new RuntimeException(e);
+	} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+	    throw new RuntimeException(e);
+	}
     }
 
     @Override
@@ -141,8 +141,7 @@ public class XmlMarketDao implements IMarketDao {
 	    Unmarshaller unmarshaller = jc.createUnmarshaller();
 	    JAXBElement<RootNode> je1 = unmarshaller.unmarshal(xml, RootNode.class);
 	    RootNode rootNode = je1.getValue();
-	    
-	    
+
 	    String fieldName = objectClass.getSimpleName();
 	    List fieldList = (List<?>) rootNode.getClass().getDeclaredField(fieldName).get(rootNode);
 	    if (fieldList == null) {
@@ -169,14 +168,13 @@ public class XmlMarketDao implements IMarketDao {
 	    throw new RuntimeException(e);
 	}
     }
-    
-    
+
     private void saveToFile(RootNode rootNode) {
 	try {
 	    JAXBContext jc = JAXBContext.newInstance(RootNode.class);
 	    JAXBElement<?> je2 = new JAXBElement(new QName(RootNode.class.getSimpleName()), RootNode.class, rootNode);
 	    Marshaller marshaller = jc.createMarshaller();
-//	    marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+	    // marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
 	    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 	    OutputStream out = new FileOutputStream(new File(db_filename), false);
 	    marshaller.marshal(je2, out);
@@ -186,14 +184,14 @@ public class XmlMarketDao implements IMarketDao {
 	    throw new RuntimeException(e);
 	}
     }
-    
+
     private RootNode getFromFile() throws JAXBException {
-	    JAXBContext jc = JAXBContext.newInstance(RootNode.class);
-	    StreamSource xml = new StreamSource(new File(db_filename));
-	    Unmarshaller unmarshaller = jc.createUnmarshaller();
-	    JAXBElement<RootNode> je1 = unmarshaller.unmarshal(xml, RootNode.class);
-	    RootNode rootNode = je1.getValue();
-	    return rootNode;
+	JAXBContext jc = JAXBContext.newInstance(RootNode.class);
+	StreamSource xml = new StreamSource(new File(db_filename));
+	Unmarshaller unmarshaller = jc.createUnmarshaller();
+	JAXBElement<RootNode> je1 = unmarshaller.unmarshal(xml, RootNode.class);
+	RootNode rootNode = je1.getValue();
+	return rootNode;
     }
 
 }
--- MarketBusinessLogic/src/main/java/market/bl/service/impl/MarketService.java
+++ MarketBusinessLogic/src/main/java/market/bl/service/impl/MarketService.java
@@ -32,11 +32,10 @@ public class MarketService implements IMarketOrderService, IMarketTaskService {
 	order.createdDate = new Date();
 	order.stopExecuted = false;
 	order.shippingExecuted = false;
-	
+
 	// validate user order for admin
 	UserOrder orderForAdmin = null;
-	if (order.userOrderList == null || order.userOrderList.size() > 1 
-		|| (orderForAdmin = order.userOrderList.get(0)) == null) {
+	if (order.userOrderList == null || order.userOrderList.size() > 1 || (orderForAdmin = order.userOrderList.get(0)) == null) {
 	    throw new MarketException("only admin user must be exist in order");
 	}
 	if (dao.get(orderForAdmin.userId, User.class) == null) {
@@ -47,31 +46,31 @@ public class MarketService implements IMarketOrderService, IMarketTaskService {
 	}
 	orderForAdmin.admin = true;
 	dao.saveOrUpdate(order);
-	
-	//create tasks
+
+	// create tasks
 	List<Task> adminTaskList = generateDefaultTaskList(order.id, orderForAdmin.userId);
-	for (Task t: adminTaskList) {
+	for (Task t : adminTaskList) {
 	    dao.saveOrUpdate(t);
 	}
-	
+
     }
 
     @Override
     public Order getOrder(int id) {
-    	return dao.get(id, Order.class);
+	return dao.get(id, Order.class);
     }
 
     @Override
     public void modifyOrder(Order order) throws MarketException {
 	Order oldOrder = getOrder(order.id);
-	
+
 	if (oldOrder == null) {
-		throw new MarketException("order with id does not exist: " + order.id);
+	    throw new MarketException("order with id does not exist: " + order.id);
 	}
 	if (oldOrder.orderState != order.orderState) {
-		throw new MarketException("you can not change order state");
+	    throw new MarketException("you can not change order state");
 	}
-	
+
 	// handle new user
 	int oldSize = oldOrder.userOrderList.size();
 	int size = order.userOrderList.size();
@@ -86,115 +85,114 @@ public class MarketService implements IMarketOrderService, IMarketTaskService {
 	    }
 
 	    if (mainTaskName != TaskName.REQUEST_PRICE_LIST && mainTaskName != TaskName.CHOICE_PRODUCTS) {
-	    	throw new MarketException("can not add new users when order is not in " 
-	    			+ TaskName.REQUEST_PRICE_LIST + TaskName.CHOICE_PRODUCTS);
+		throw new MarketException("can not add new users when order is not in " + TaskName.REQUEST_PRICE_LIST + TaskName.CHOICE_PRODUCTS);
 	    }
 
 	    for (int i = oldSize; i < size; i++) {
-			UserOrder partOrder = order.userOrderList.get(i);
-			if (dao.get(partOrder.userId, User.class) == null) {
-			    throw new MarketException("user must be exist in database");
-			}
-			partOrder.admin = false;
+		UserOrder partOrder = order.userOrderList.get(i);
+		if (dao.get(partOrder.userId, User.class) == null) {
+		    throw new MarketException("user must be exist in database");
+		}
+		partOrder.admin = false;
 
-			// add tasks for new user
-			List<Task> adminTaskList = generateDefaultTaskList(order.id, partOrder.userId);
-			if (mainTaskName == TaskName.CHOICE_PRODUCTS) {
-			    adminTaskList.get(0).taskState = TaskState.PASSED;
-			    adminTaskList.get(1).taskState = TaskState.RUNNING;
-			}
-			for (Task t: adminTaskList) {
-			    dao.saveOrUpdate(t);
-			}
+		// add tasks for new user
+		List<Task> adminTaskList = generateDefaultTaskList(order.id, partOrder.userId);
+		if (mainTaskName == TaskName.CHOICE_PRODUCTS) {
+		    adminTaskList.get(0).taskState = TaskState.PASSED;
+		    adminTaskList.get(1).taskState = TaskState.RUNNING;
+		}
+		for (Task t : adminTaskList) {
+		    dao.saveOrUpdate(t);
+		}
 	    }
 	}
 	dao.saveOrUpdate(order);
     }
-    
+
     public List<Task> getTaskList(int orderId, int userId) throws MarketException {
-    	// SELECT task t from Task t WHERE t.orderId = :orderId and t.userId =: userId
-    	List<Task> retVal = new ArrayList<Task>();
-    	List<Task> allTask = dao.getAll(Task.class);
-    	// dirty hack, need field "order" in task. Now it sorted by id
-    	Collections.sort(allTask, new Comparator<Task>() {
-			@Override
-			public int compare(Task o1, Task o2) {
-				if (o1.id > o2.id)
-					return 1;
-				else
-					return -1;
-			}
-		});
-    	
-    	for (Task t: allTask) {
-    		if (t.orderId == orderId && t.userId == userId) {
-    			retVal.add(t);
-    		}
-    	}
-    	return retVal;
+	// SELECT task t from Task t WHERE t.orderId = :orderId and t.userId =:
+	// userId
+	List<Task> retVal = new ArrayList<Task>();
+	List<Task> allTask = dao.getAll(Task.class);
+	// dirty hack, need field "order" in task. Now it sorted by id
+	Collections.sort(allTask, new Comparator<Task>() {
+	    @Override
+	    public int compare(Task o1, Task o2) {
+		if (o1.id > o2.id)
+		    return 1;
+		else
+		    return -1;
+	    }
+	});
+
+	for (Task t : allTask) {
+	    if (t.orderId == orderId && t.userId == userId) {
+		retVal.add(t);
+	    }
+	}
+	return retVal;
     }
 
     @Override
     public void resumeTask(Task task) throws MarketException {
-    	Order order = dao.get(task.orderId, Order.class);
-    	
-		UserOrder userOrder = null;
+	Order order = dao.get(task.orderId, Order.class);
+
+	UserOrder userOrder = null;
+	for (UserOrder p : order.userOrderList) {
+	    if (p.userId == task.userId) {
+		userOrder = p;
+	    }
+	}
+
+	// handle admin's task
+	if (task.taskName == TaskName.REQUEST_PRICE_LIST) {
+	    if (userOrder.admin) {
+		// admin
+		if (task.priceListUrl == null || task.priceListUrl.isEmpty()) {
+		    throw new MarketException("price list url must be filled");
+		}
+		// go to next task for all users
 		for (UserOrder p : order.userOrderList) {
-		    if (p.userId == task.userId) {
-			userOrder = p;
+		    List<Task> taskList = getTaskList(task.orderId, p.userId);
+		    for (int i = 0; i < taskList.size() - 1; i++) {
+			Task t = taskList.get(i);
+			Task tNext = taskList.get(i + 1);
+			if (t.taskName == TaskName.REQUEST_PRICE_LIST && t.taskState == TaskState.RUNNING) {
+			    t.taskState = TaskState.PASSED;
+			    tNext.taskState = TaskState.RUNNING;
+			    dao.saveOrUpdate(t);
+			    dao.saveOrUpdate(tNext);
+			}
 		    }
 		}
-
-		// handle admin's task
+	    } else {
+		// no admin
 		if (task.taskName == TaskName.REQUEST_PRICE_LIST) {
-			if (userOrder.admin) {
-				// admin
-				if (task.priceListUrl == null || task.priceListUrl.isEmpty()) {
-					throw new MarketException("price list url must be filled");
-				}
-				// go to next task for all users
-				for (UserOrder p: order.userOrderList) {
-					List<Task> taskList = getTaskList(task.orderId, p.userId);
-					for (int i = 0; i < taskList.size() - 1; i++) {
-						Task t = taskList.get(i);
-						Task tNext = taskList.get(i + 1);
-						if (t.taskName == TaskName.REQUEST_PRICE_LIST && t.taskState == TaskState.RUNNING) {
-							t.taskState = TaskState.PASSED;
-							tNext.taskState = TaskState.RUNNING;
-							dao.saveOrUpdate(t);
-							dao.saveOrUpdate(tNext);
-						}
-					}
-				}
-			} else {
-				// no admin
-				if (task.taskName == TaskName.REQUEST_PRICE_LIST) {
-					throw new MarketException("task " + TaskName.REQUEST_PRICE_LIST 
-							+ " can be resumed only by admin");
-				}
-			}
-			
-		} else if (task.taskName == TaskName.CHOICE_PRODUCTS) {
-			List<Task> taskList = getTaskList(task.orderId, task.userId);
-			for (int i = 0; i < taskList.size(); i++) {
-				if (taskList.get(i).id == task.id) {
-					taskList.get(i).taskState = TaskState.PASSED;
-					taskList.get(i + 1).taskState = TaskState.RUNNING;
-					dao.saveOrUpdate(taskList.get(i));
-					dao.saveOrUpdate(taskList.get(i + 1));
-				}
-			}
-			
-		} else if (task.taskName == TaskName.WAITING_RESPONCE_FROM_FIRM) {
-		} else if (task.taskName == TaskName.WAITING_BITTING) {
-		} else if (task.taskName == TaskName.SHIPPING) {
+		    throw new MarketException("task " + TaskName.REQUEST_PRICE_LIST + " can be resumed only by admin");
 		}
+	    }
+
+	} else if (task.taskName == TaskName.CHOICE_PRODUCTS) {
+	    List<Task> taskList = getTaskList(task.orderId, task.userId);
+	    for (int i = 0; i < taskList.size(); i++) {
+		if (taskList.get(i).id == task.id) {
+		    taskList.get(i).taskState = TaskState.PASSED;
+		    taskList.get(i + 1).taskState = TaskState.RUNNING;
+		    dao.saveOrUpdate(taskList.get(i));
+		    dao.saveOrUpdate(taskList.get(i + 1));
+		}
+	    }
+
+	} else if (task.taskName == TaskName.WAITING_RESPONCE_FROM_FIRM) {
+	} else if (task.taskName == TaskName.WAITING_BITTING) {
+	} else if (task.taskName == TaskName.SHIPPING) {
+	}
     }
 
     public IMarketDao getDao() {
-        return dao;
+	return dao;
     }
-    
+
     private List<Task> generateDefaultTaskList(int orderId, int userId) {
 	int taskStartId = orderId * 1000 + userId * 100;
 	Task task0 = new Task();
@@ -203,28 +201,28 @@ public class MarketService implements IMarketOrderService, IMarketTaskService {
 	task0.userId = userId;
 	task0.taskName = TaskName.REQUEST_PRICE_LIST;
 	task0.taskState = TaskState.RUNNING;
-	
+
 	Task task1 = new Task();
 	task1.id = taskStartId + 1; // lastId
 	task1.orderId = orderId;
 	task1.userId = userId;
 	task1.taskName = TaskName.CHOICE_PRODUCTS;
 	task1.taskState = TaskState.NOT_STARTED;
-	
+
 	Task task2 = new Task();
 	task2.id = taskStartId + 2; // lastId
 	task2.orderId = orderId;
 	task2.userId = userId;
 	task2.taskName = TaskName.WAITING_RESPONCE_FROM_FIRM;
 	task2.taskState = TaskState.NOT_STARTED;
-	
+
 	Task task3 = new Task();
 	task3.id = taskStartId + 3; // lastId
 	task3.orderId = orderId;
 	task3.userId = userId;
 	task3.taskName = TaskName.WAITING_BITTING;
 	task3.taskState = TaskState.NOT_STARTED;
-	
+
 	Task task4 = new Task();
 	task4.id = taskStartId + 4; // lastId
 	task4.orderId = orderId;
@@ -234,6 +232,5 @@ public class MarketService implements IMarketOrderService, IMarketTaskService {
 
 	return Arrays.asList(task0, task1, task2, task3, task4);
     }
-    
 
 }
